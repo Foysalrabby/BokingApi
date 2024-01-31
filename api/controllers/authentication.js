4341d83d571bf9e736b62b557cb12ea8bb997aa6
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-01-31 23:42:35 +0600

    for seurity purose

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -2,10 +2,16 @@ import User from "../Model/User.js";
 
 //create 
 export const createUserAUTH = async(req,res,next) => {
-    const authusers= new User(req.body);
+    
 try {
-    const saveUser=await authusers.save();
-    return res.status(200).send(saveUser);
+    const authusers= new User({
+        
+       username:req.body.username,
+       useremail:req.body.useremail,
+       password:req.body.password
+    });
+    await authusers.save();
+    return res.status(200).send("user is create successfully");
    
 } catch (error) {
    next(error);
@@ -13,3 +19,12 @@ try {
 } 
 }
 //update 
+
+
+//delete
+
+
+// getbyid
+
+//getAll
+
