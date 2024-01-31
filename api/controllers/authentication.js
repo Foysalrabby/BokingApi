5680af045e43102e240af9b72602f9a2f2e1347f
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-01 01:26:58 +0600

    openssl

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,6 +1,7 @@
 import User from "../Model/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../Untils/error.js";
+import jwt from "jsonwebtoken";
 
 //create registration 
 export const createUserAUTH = async(req,res,next) => {
@@ -8,7 +9,7 @@ export const createUserAUTH = async(req,res,next) => {
 try {
 
     var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(req.body.password, salt); 
+    var hash = bcrypt.hashSync(req.body.password, salt);  //bcryptjs
     const authusers= new User({
         
        username:req.body.username,
@@ -28,16 +29,19 @@ try {
 
 export const createusrlogin=async(req,res,next) =>{
 try {
-    
+     //bcryptjs
 const usercheck= await User.findOne({username:req.body.username})
 if(!usercheck){return res.status(404).send("Username is not found")}
 
 const passwordcheck= await bcrypt.compareSync(req.body.password, usercheck.password) 
 if(!passwordcheck){return next(createError("404","password is not coorect")) }
 
-const {username,password, ...otheedeails}=usercheck._doc;
+//jwt cookies and token jsonwebtoken
 
+const {username,password, ...otheedeails}=usercheck._doc;
 res.status(200).json({...otheedeails});
+ //jwtwebtoken
+var token = jwt.sign({ id:usercheck._id,isAdmin:usercheck.isAdmin}, 'shhffder');
 
 } catch (error) {
     next(error); 
