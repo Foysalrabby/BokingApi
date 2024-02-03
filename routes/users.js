1081bd3bd8f9753d32925e3df0c60ca5704611f5
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-04 01:41:58 +0600

    varify token and login

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,42 @@
-import  express from "express";
+import express from "express";
 import User from "../api/Model/User.js";
 import { createUser, deletUser, getAlluser, getidUser, updatUser } from "../api/controllers/user.js";
+import { Varifyctoken } from "../api/Untils/Varifytoken.js";
 const router=express.Router();
 
 
+//varify token and middleware to access other route
 
+router.get("/checkloginToken",Varifyctoken,(req,res,next)=>{
+    res.send("hey you are login");
+})
 //create post
 router.post("/",createUser);//create
+
 router.put("/:id",updatUser); //update
+
 router.delete("/:id",deletUser);//delete
+
 router.get("/:id",getidUser);//getbyid
+
 router.get("/",getAlluser);//get all
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
