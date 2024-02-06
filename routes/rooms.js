6b92b54067ff6c2aca14b7061cf1d0118d9361a9
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-07 02:11:32 +0600

    to take update room and createrooms in controller from routes

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,8 +1,29 @@
 import  express from "express";
-
-
+import Room from "../api/Model/Room.js";
 const router=express.Router();
-router.get("/",(req,res)=>{
-res.send("hello this is room method");
+
+//create rooms
+router.post("/",async(req,res,next) => { 
+     const Roominfo=new Room(req.body);
+    try {
+          const saveRoom=await Roominfo.save();
+          res.status(200).send(saveRoom);
+        
+    } catch (error) {
+       next(error); 
+    }
+
 })
+//update
+router.put("/:id",async(req,res,next) =>{
+    try {
+        const UpdateRooms=await Room.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true});
+        res.status(202).send(UpdateRooms);
+        
+    } catch (error) {
+        next(error);
+    }
+})
+
+
 export default router;
