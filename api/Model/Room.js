5570c9b890ade5f04677fee7e9c1b89061a29f1d
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-07 01:46:31 +0600

    create room schema

---
+++ api/Model/Room.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const Roomschema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true 
+    },
+   
+    describtion:{
+        type:String,
+        required:true 
+    },
+    maxpeople:{
+        type:Number,
+        required:true 
+    },
+    roomnum:[{number:Number,unavialabledate:{type:[Date]}}],
+   
+       
+},
+{timestamps:true});
+export default mongoose.model("Rooms",Roomschema);
