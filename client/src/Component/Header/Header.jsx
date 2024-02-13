09b82c13c5635fb87b8740b9f9b81061925d3645
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-14 00:18:17 +0600

    searchmenu design

--- client/src/Component/Header/Header.jsx
+++ client/src/Component/Header/Header.jsx
@@ -5,7 +5,8 @@ import { RiFlightTakeoffLine } from "react-icons/ri";
 import { FaCarSide } from "react-icons/fa";
 import { HiMiniHomeModern } from "react-icons/hi2";
 import { RiTaxiFill } from "react-icons/ri";
-
+import { MdDateRange } from "react-icons/md";
+import { FaPeopleRoof } from "react-icons/fa6";
 const Header = () => {
     return (
         <div className='Header'>
@@ -35,8 +36,27 @@ const Header = () => {
         <h2 className='headerTittle'>A Life Time Discount? It's Genius</h2>
        <p className='headerdes'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
        Cumque eius optio amet facere ut nisi culpa quaerat explicabo quidem perferendis.</p>
-    
         <button className='navbutton'> SingIn/Register</button>
+
+        <div className="HeaderSearch">
+        <div className="HeaderSearchitem">
+          <MdHotel className='headericon' />
+       <input type="text" placeholder='Where Are You Going ?' className='inputseacrheader' />
+         </div>
+         <div className="HeaderSearchitem">
+          <MdDateRange  className='headericon'/>
+         <span className='headertext'>Date To Date</span>
+         </div>
+         <div className="HeaderSearchitem">
+          <FaPeopleRoof  className='headericon'/>
+          <span className='headertext'>2 Adult 2 Children 1 Room </span>
+         </div>
+         <div className="HeaderSearchitem">
+          <button className='navbutton'>Search</button>
+         </div>
+
+        </div> 
+
         </div>   
         </div>
     );
