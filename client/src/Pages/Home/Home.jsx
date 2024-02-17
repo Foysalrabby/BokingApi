1141724fd87ce75d833aafe01b24a80ca6b7a888
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-18 00:27:58 +0600

    create features page and start up

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../Component/Navbar/Navbar';
 import Header from '../../Component/Header/Header';
+import Features from '../../Component/Features/Features';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Header> </Header>
+            <Features/>
         </div>
     );
 };
