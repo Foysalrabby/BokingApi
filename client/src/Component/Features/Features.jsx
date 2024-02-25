7d6a2dd9b845ea8526b835c01680ebb13aa6bcdf
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-26 02:12:14 +0600

    browse type design with featurelist

--- client/src/Component/Features/Features.jsx
+++ client/src/Component/Features/Features.jsx
@@ -3,8 +3,63 @@ import './Features.css';
 
 const Features = () => {
     return (
-        <div>
-            <h1>Features</h1>
+        <div className='featured'>
+        <div className="featureditem">
+        {/* <img src="https://drive.google.com/uc?export=view&id=1mMbf2kzrbCyBrjx1VxSV05XfxDDxlnN9"
+          alt="Drive Preview"/> */}
+          {/* <img src={require("../../images/dhaka.jpg")} width="260px" height="260px" alt="dhaka" /> */}
+          <img src={"https://img.freepik.com/premium-photo/city-skyline-with-single-tree-growing-out-it-representing-balance-urbanization-nature-generative-ai_864588-3403.jpg?w=740"}
+           alt="m" className='featureImg' />
+          <div className="features_title">
+            <h1>Rajshahi </h1>
+            <h1>123 properties </h1>
+          </div>
+        </div>
+
+        <div className="featureditem">
+          <img src={"http://samogoo.net/uploads/a6/a637869bd067c0f01b3560403cf5ea65_2014-12-19_11-45-27.jpg"}
+           alt="m" className='featureImg' />
+          <div className="features_title">
+            <h1>Dhaka </h1>
+            <h1>453 properties </h1>
+          </div>
+        </div>
+          
+
+        <div className="featureditem">
+          <img src={"https://i0.hippopx.com/photos/945/657/311/slovenia-birds-field-tree-preview.jpg"}
+           alt="m" className='featureImg' />
+          <div className="features_title">
+            <h1>khulna </h1>
+            <h1>453 properties </h1>
+          </div>
+        </div>
+
+        <div className="featureditem">
+          <img src={"http://ro-pskov.ru/wp-content/uploads/2020/07/naberezhnayapskovy2.jpg"}
+           alt="m" className='featureImg' />
+          <div className="features_title">
+            <h1>Borishal </h1>
+            <h1>453 properties </h1>
+          </div>
+        </div>
+        <div className="featureditem">
+          <img src={"http://samogoo.net/uploads/a6/a637869bd067c0f01b3560403cf5ea65_2014-12-19_11-45-27.jpg"}
+           alt="m" className='featureImg' />
+          <div className="features_title">
+            <h1>Sylhet </h1>
+            <h1>453 properties </h1>
+          </div>
+        </div>
+        <div className="featureditem">
+          <img src={"http://ro-pskov.ru/wp-content/uploads/2020/07/naberezhnayapskovy2.jpg"}
+           alt="m" className='featureImg' />
+          <div className="features_title">
+            <h1>Rongpur </h1>
+            <h1>453 properties </h1>
+          </div>
+        </div>
+        
         </div>
     );
 };
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import Navbar from '../../Component/Navbar/Navbar';
 import Header from '../../Component/Header/Header';
 import Features from '../../Component/Features/Features';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Header> </Header>
+            <div className="homecontainer">
             <Features/>
+            
+            </div>
+            
         </div>
     );
 };
