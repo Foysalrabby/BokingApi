d419e7d68a2629353113e948966005016cb7feed
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-11-10 00:13:40 +0600

    set username in  after login

--- client/src/Component/Navbar/Navbar.jsx
+++ client/src/Component/Navbar/Navbar.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './navbar.css';
 import { Link } from "react-router-dom";
+import { AuthLogincontext } from '../../context/Authentication';
 
 const Navbar = () => {
+    const {user}=useContext(AuthLogincontext);
+    //console.log(user.data.username);
     return (
         <div className='navbar'>
         <div className='navContainer'>
         <Link to="/" style={{color:'white',textDecoration:'none'}}>
         <span className='logo'>Lamababooking</span>
         </Link>
-        <div className='navItem'>
+        {user ? user.data.username :(<div className='navItem'>
         <button className='navButton'> Singup</button>
         <button className='navButton'> LogIn</button>
        
-        </div>
+        </div>)}
         </div>
         </div>
     );
