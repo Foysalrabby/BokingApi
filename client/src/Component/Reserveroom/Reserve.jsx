24eee395371b5cec499f50996fbb719dc1c36628
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-11-22 01:17:31 +0600

    start to reserve book buy button work

--- client/src/Component/Reserveroom/Reserve.jsx
+++ client/src/Component/Reserveroom/Reserve.jsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Reserve.css';
 import { RxCross2 } from "react-icons/rx";
 import useFetchdata from '../../Hooks/Usefetchdata';
 
 const Reserve = ({seuloginreserve,hotelid}) => {
     const {data,load,error,refetch}=useFetchdata(`/hotels/room/${hotelid}`);
-    
-
     console.log(data);
+ const [selectrooms,setselectedroom] =useState([]);   
+ const handlesclect=(e)=>{
+         const checked=e.target.checked;
+         var values=e.target.value;
+         setselectedroom(checked ? [...selectrooms,values] :selectrooms.filter((item)=> item !== values) );
+    }
+    console.log(selectrooms);
+
     return (
         <div className='reservecontainer'>
         <div className="reservesubcontainer">
         <RxCross2 className='rsserveicon' onClick={()=>seuloginreserve(false)}/>
 
            <span> Select your room:</span>
-           {data.rooms?.map((item)=> (<div className='Ritem' key={item.hotelid}>
+           {data.rooms?.map((item)=> (<div className='Ritem' key={item._id}>
             <div className="Riteminfo">
             <div className="rtittle">{item.title}</div>
             <div className="rprice">{item.price}</div>
             <div className="rdescribtion">{item.describtion}</div>
             <div className="rmaxpeople"> max people {item.maxpeople}</div>
             </div>
+            {
+                item.roomnum.map((roomnumid) =>(<div className='roomid' key={roomnumid._id}>
+                <label>{roomnumid.number}</label>
+                <input type='checkbox' value={roomnumid._id} onChange={handlesclect}/>
+                </div>))
+            }
             
             </div>))
            }
