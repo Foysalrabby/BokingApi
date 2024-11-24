46ae366ee18e712d92834b3c12c58fbd0f139a43
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-11-24 23:15:42 +0600

    sucessfully add data for room number is available or not

--- client/src/Component/Reserveroom/Reserve.jsx
+++ client/src/Component/Reserveroom/Reserve.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Reserve.css';
 import { RxCross2 } from "react-icons/rx";
 import useFetchdata from '../../Hooks/Usefetchdata';
+import { SearchContext } from '../../context/Searchcontext';
 
 const Reserve = ({seuloginreserve,hotelid}) => {
     const {data,load,error,refetch}=useFetchdata(`/hotels/room/${hotelid}`);
-    console.log(data);
+    //console.log(data);
  const [selectrooms,setselectedroom] =useState([]);   
+ const {dates}=useContext(SearchContext);
  const handlesclect=(e)=>{
          const checked=e.target.checked;
          var values=e.target.value;
          setselectedroom(checked ? [...selectrooms,values] :selectrooms.filter((item)=> item !== values) );
     }
     console.log(selectrooms);
+    const getrange = (startDate, endDate) => {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        const date = new Date(start?.getTime());
+        const dates = [];
+        while (date < end) { // Corrected typo here
+            dates.push(new Date(date)?.getTime());
+            date.setDate(date?.getDate() + 1);
+        }
+        return dates;
+    };
+    console.log(getrange(dates[0]?.startDate,dates[0]?.endDate));
+    const handliclick=()=>{
+
+    }
 
     return (
         <div className='reservecontainer'>
@@ -36,6 +53,7 @@ const Reserve = ({seuloginreserve,hotelid}) => {
             
             </div>))
            }
+           <button className='Resevebtn' onClick={handliclick}></button>
 
         </div>
         </div>
